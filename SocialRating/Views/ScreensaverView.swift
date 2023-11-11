ada8e0b4495659a788b10c24203b6aa9b178870e
TimurKhazeev <timurhazeev69252@icloud.com>
2023-11-12 01:02:18 +0500

    The screensaver is written

--- SocialRating/Views/ScreensaverView.swift
+++ SocialRating/Views/ScreensaverView.swift
@@ -4,15 +4,54 @@
 //
 //  Created by Тимур Хазеев on 11.11.2023.
 //
+// Заставка
 
 import SwiftUI
 
+/// Этот класс представляет экран заставки приложения.
+/// Он отображает анимированный логотип до активации основного представления.
 struct ScreensaverView: View {
-    var body: some View {
-        Text("Hi")
+  
+  @State private var isActiv = false
+  @State private var size = 0.8
+  @State private var opacity = 0.5
+  
+  var body: some View {
+    if isActiv {
+      MainView()
+    } else {
+      
+      VStack {
+        VStack {
+          Image("logoRed")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 350, height: 350)
+          
+        }
+        .scaleEffect(size)
+        .opacity(opacity)
+        .onAppear() {
+          withAnimation(.easeIn(duration: 1.2)) {
+            self.size = 0.9
+            self.opacity = 1.0
+          }
+        }
+        
+      }
+      .onAppear() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+          withAnimation() {
+            self.isActiv = true
+          }
+        }
+      }
+      
     }
+    
+  }
 }
 
 #Preview {
-    ScreensaverView()
+  ScreensaverView()
 }
