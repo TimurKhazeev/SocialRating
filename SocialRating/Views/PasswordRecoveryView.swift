e56b2e98028fab781e390be069575dbb071d3a34
TimurKhazeev <timurhazeev69252@icloud.com>
2023-11-13 01:56:41 +0500

    The login page has been written

---
+++ SocialRating/Views/PasswordRecoveryView.swift
@@ -0,0 +1,18 @@
+//
+//  PasswordRecoveryView.swift
+//  SocialRating
+//
+//  Created by Тимур Хазеев on 13.11.2023.
+//
+
+import SwiftUI
+
+struct PasswordRecoveryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PasswordRecoveryView()
+}
