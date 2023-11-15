315cc214131dcaf28bbe9e9b6020b1bd35ec69f6
TimurKhazeev <timurhazeev69252@icloud.com>
2023-11-16 03:09:18 +0500

    Added small checks for correct input and preparation for json

---
+++ SocialRating/Views/SRButton.swift
@@ -0,0 +1,18 @@
+//
+//  SRButton.swift
+//  SocialRating
+//
+//  Created by Тимур Хазеев on 16.11.2023.
+//
+
+import SwiftUI
+
+struct SRButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SRButton()
+}
